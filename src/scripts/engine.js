86a841b32d9cea0af3098dedbdbdbe1472e5e686
Vinicius Souza <viniciussouzaesp@gmail.com>
2024-12-14 21:26:02 -0300

    Finalização do html e css, começo do development da engine

---
+++ src/scripts/engine.js
@@ -0,0 +1,50 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    values:{
+        timerId: null,
+        countDownTimerId: setInterval(countDown, 1000),
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+    },
+};
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime <= 0) {
+        alert("Game Over! O seu resultado foi: " + state.values.result);
+    }
+}
+
+function randomSquare(){
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+}
+
+function moveEnemy() {
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+}
+
+function addListener(){
+    state.view.squares.forEach((square) => {});
+}
+
+function init() {
+    moveEnemy();
+}
+
+
+init();
